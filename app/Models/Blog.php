874d19724bee7b35eea698b47addc18fcb50d31d
mny7240 <mana.078.noa@gmail.com>
2023-08-22 05:27:00 +0000

    ブログ投稿の編集追加

---
+++ app/Models/Blog.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    // テーブル名
+    protected $table = 'blogs';    // table と Blog が紐づけられた
+
+    // 可変項目
+    protected $fillable =
+    [
+        'title',
+        'content'
+    ]
+
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
-<?php
+<?php              //ここにはルーティング(ブラウザから任意のアクセスがあった場合にどのcontroller処理を動かすのかを記述する)について書く
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+@@ -13,6 +14,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);  //ちなみに基本的にアプリを起動してブラウザから送られてくる最初のリクエストはこの  /   というURLである。
+Route::get('/posts/create',[PostController::class,'create']);
+Route::get('/posts/{post}',[PostController::class, 'show']);    // /{post}はルートパラメータといい、postのidは選んだ投稿によって変わってしまうため、{post}にする。
+Route::post('/posts',[PostController::class,'store']);    //storeはデータを保存するという意味
+Route::get('/posts/{post}/edit',[PostController::class, 'edit']);
+Route::get('posts/{post}',[PostController::class, 'update']);
+
+
+// Route･･･laravel 既存機能のRouteファサードを使う
+// get･･･HTTP メソッドを指定
+// /･･･任意の URL を指定
+// 
